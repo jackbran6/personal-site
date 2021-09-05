cc219a8496e3ecee9543d6ee5fd06de55496c07b
Jack <jack.branthwaite@gmail.com>
2021-09-05 15:40:29 +1200

    Start of portfolio page

---
+++ src/components/portfolio-card/PortfolioCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <h6 class="title">This is a title</h6>
+    <p class="description">
+      This is a short description about the project that is being displayed here
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PortfolioCard'
+})
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: #000 solid 1px;
+  min-width: 300px;
+  padding: 2vw;
+}
+</style>
--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,23 +1,49 @@
 <template>
   <div class="container">
-    <h1 class="title">work</h1>
+    <h1 class="title">portfolio</h1>
+    <div class="card-container">
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+      <portfolio-card />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import PortfolioCard from '~/components/portfolio-card/PortfolioCard.vue'
 
 export default Vue.extend({
-  name: 'Work',
-
-  head: {
-    title: 'work'
-  }
+  name: 'Portfolio',
+  components: { PortfolioCard }
 })
 </script>
 
 <style lang="scss" scoped>
+@import '~assets/styles/config';
 .container {
   margin: auto;
   width: 80vw;
+}
+.card-container {
+  display: grid;
+  gap: 1vw;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin-top: 2vw;
+}
+@include tablet {
+  .card-container {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+@include mobile-l {
+  .card-container {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
